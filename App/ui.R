aa2d6b69aab1ba376a1b17f0a30298fee945d1b1
Matthew Lopes <mattlopes1997@gmail.com>
2022-11-28 19:18:35 -0500

    initial commit

---
+++ App/ui.R
@@ -0,0 +1,61 @@
+## ui.R
+library(shiny)
+library(shinydashboard)
+library(recommenderlab)
+library(data.table)
+library(ShinyRatingInput)
+library(shinyjs)
+
+source('functions/helpers.R')
+
+shinyUI(dashboardPage(
+  skin = "blue",
+  dashboardHeader(title = "Movie Recommender"),
+  
+  dashboardSidebar(sidebarMenu(
+    menuItem("By Genre", tabName = "genre", icon = icon("dashboard")),
+    menuItem("By Rating", tabName = "rating", icon = icon("th"))
+  )),
+  
+  dashboardBody(includeCSS("css/movies.css"),
+                tabItems(
+                  tabItem(tabName = "genre",
+                          h2("Recommendations by Genre"),
+                          selectInput("genre", "Genre:",
+                                      c("Cylinders" = "cyl",
+                                        "Transmission" = "am",
+                                        "Gears" = "gear")),
+                          radioButtons("top5", "Top 5:",
+                                       c("Most Popular" = "popular",
+                                         "Highest Rating" = "rating")),
+                  ),
+                  
+                  tabItem(tabName = "rating",
+                          fluidRow(
+                            box(
+                              width = 12,
+                              title = "Step 1: Rate as many movies as possible",
+                              status = "info",
+                              solidHeader = TRUE,
+                              collapsible = TRUE,
+                              div(class = "rateitems",
+                                  uiOutput('ratings'))
+                            )
+                          ),
+                          fluidRow(
+                            useShinyjs(),
+                            box(
+                              width = 12,
+                              status = "info",
+                              solidHeader = TRUE,
+                              title = "Step 2: Discover movies you might like",
+                              br(),
+                              withBusyIndicatorUI(
+                                actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                              ),
+                              br(),
+                              tableOutput("results")
+                            )
+                          ))
+                ))
+)) 
